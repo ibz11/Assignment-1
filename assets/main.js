560fbdbdbf67d8e5980900f3a940d4dc900d8efc
Ibrahim <ibramiabdi.ke@gmail.com>
2022-06-20 21:59:42 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+
+  
+  document.getElementById("btn").onclick = function(){
+
+    var name = document.getElementById("name").value;
+   
+    document.getElementById("txt").innerHTML = "Hello " + name + " ! " + " Have a great day";
+
+}
